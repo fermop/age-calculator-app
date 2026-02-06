8e8af21c45fd5ea7e536b3cdb2706f5a5d6f161f
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-02-05 18:36:35 -0600

    fix: add semantic tags and a level-one heading tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import Card from "@/components/Card";
 export default function Home() {
   return (
     <div className="w-full p-4">
-      <Card />
+      <main>
+        <h1 className="sr-only">
+          Frontend Mentor | Age calculator app
+        </h1>
+
+        <Card />
+      </main>
     </div>
   );
 }
